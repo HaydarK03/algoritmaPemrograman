0b9e952a4b68bcd77d5f21abafa9222c44556d8d
hayskar <kamilhaydar03@gmail.com>
2023-11-18 01:19:17 +0700

    add script

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-// test
+var input = prompt("masukan jumlah penghasilan anda perbulan:")
+var x = 25
+var y = 1000 
+var z = x/y
+
+
+function hasil(){
+    var hitung = input * z
+    return hitung
+}
+alert("zakat yang ha    rus dibayarkan :" + hasil())
