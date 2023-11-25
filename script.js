391e684e0787893ec3b98e194e158b49a8f4ac9a
haydark03 <kamilhaydar03@gmail.com>
2023-11-25 15:09:42 +0700

    add new js

--- script.js
+++ script.js
@@ -1,11 +1,35 @@
-var input = prompt("masukan jumlah penghasilan anda perbulan:")
+// var input = prompt("masukan jumlah penghasilan anda perbulan:")
 var x = 25
 var y = 1000 
 var z = x/y
 
 
-function hasil(){
-    var hitung = input * z
-    return hitung
+// function hasil(){
+//     var hitung = input * z
+//     return hitung
+// }
+// alert("zakat yang ha    rus dibayarkan :" + hasil())
+
+
+// ambil  variabel untuk menampung calue
+
+
+let inputText = document.getElementById("penghasilan")
+let inputButton = document.getElementById("button")
+let print = document.getElementById("hasil")
+
+// function getValue(){
+//     alert(inputText.value);
+// }
+// inputButton.addEventListener("click",getValue)
+
+
+function hitung(){
+    var hasil = inputText.value * z
+    console.log(hasil)
+
 }
-alert("zakat yang ha    rus dibayarkan :" + hasil())
+
+inputButton.addEventListener("click", hitung)
+
+
