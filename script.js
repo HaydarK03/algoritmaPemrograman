d4b1c0cbf20d99ed36e1fc02e773b24e95e3b65b
haydark03 <kamilhaydar03@gmail.com>
2023-12-01 17:33:02 +0700

    fix JS and fix UI

--- script.js
+++ script.js
@@ -1,28 +1,32 @@
 var x = 25;
 var y = 1000 ;
 var z = x/y;
-var a = 7000000
+var a = 7897619
 
 let inputText = document.getElementById("penghasilan");
 let inputButton = document.getElementById("button");
 let reset = document.getElementById('reset');
+let output = document.getElementById('hasil');
 
 // untuk perhitungan
 function getHitung(){
-    var hasil = inputText.value * z
-    return hasil
+        var hasil = inputText.value * z
+        return hasil
 };
 
 // untuk menampilkan value
 inputButton.addEventListener('click', function(){
-    document.getElementById("hasil").innerText = getHitung()
+    if (inputText.value > a)  {
+        output.innerText = getHitung()
+    }
+    else {
+        output.innerText = 0
+        alert('Anda tidak wajib membayar zakat')
+    }
 });
+
 
 // untuk mereset value
 reset.addEventListener('click', function(){
-    document.getElementById('hasil').innerText = 0
+    output.innerText = 0
 });
-
-if (inputText.value < a){
-    document.getElementById('hasil').innerText = 0
-};
